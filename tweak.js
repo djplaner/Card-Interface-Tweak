af0558b5c2170eec12739cbc32d388434070d139
David Jones <davidthomjones@gmail.com>
2019-01-02 13:35:38 +1000

    Added link to readme

--- tweak.js
+++ tweak.js
@@ -1,11 +1,10 @@
 <p><strong>Important:</strong> Do not edit or remove content from this item. It implements the Card interface. Changes may break that interface.</p>
+    <p>See <a href="https://github.com/djplaner/Card-Interface-Tweak/blob/master/README.md">this README file</a> for insight on how to use this tweak.</p>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js" type="text/javascript"></script>
 <script src="https://djon.es/gu/cards.js" type="text/javascript"></script>
 <script type="text/javascript" class="gutweak"><![CDATA[
 $.noConflict(); 
-
 jQuery( document ).ready(function( $ ) { 
-      
   cardsInterface($);
 });
 ]]></script>
