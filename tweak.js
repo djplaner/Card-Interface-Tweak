e345a26a345bc009ac2c198d6126b998cbdf2e31
David Jones <davidthomjones@gmail.com>
2019-01-02 13:37:33 +1000

    Use _blank to support Bb restrictions

--- tweak.js
+++ tweak.js
@@ -1,5 +1,5 @@
 <p><strong>Important:</strong> Do not edit or remove content from this item. It implements the Card interface. Changes may break that interface.</p>
-    <p>See <a href="https://github.com/djplaner/Card-Interface-Tweak/blob/master/README.md">this README file</a> for insight on how to use this tweak.</p>
+    <p>See <a target="_blank" href="https://github.com/djplaner/Card-Interface-Tweak/blob/master/README.md">this README file</a> for insight on how to use this tweak.</p>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js" type="text/javascript"></script>
 <script src="https://djon.es/gu/cards.js" type="text/javascript"></script>
 <script type="text/javascript" class="gutweak"><![CDATA[
