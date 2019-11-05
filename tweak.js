de0bda5f6f5c19e3e563fab3f3a808b477ec6f65
David Jones <davidthomjones@gmail.com>
2019-11-06 07:47:57 +1000

    Updated instructions on the tweak

--- tweak.js
+++ tweak.js
@@ -1,4 +1,8 @@
-<p><strong>Important:</strong> Do not edit or remove content from this item. It implements the Card interface. Changes may break that interface.</p>
+<p><strong>Important:</strong> </p>
+<ol>
+  <li> <strong>No changes</strong> - do not edit or remove content from this item. It implements the Card interface. Changes may break that interface.</li>
+  <li> <strong>Don't hide it from students</strong> - the tweak will hide itself, students won't see it. If you use Blackboard to hide this tweak from students, it will NOT work.</li>
+  </ol>
     <p>See <a target="_blank" href="https://github.com/djplaner/Card-Interface-Tweak/blob/master/README.md">this README file</a> for insight on how to use this tweak.</p>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js" type="text/javascript"></script>
 <script src="https://s3.amazonaws.com/filebucketdave/banner.js/cards.js" type="text/javascript"></script>
