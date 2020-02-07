6d90d8cd8a99c839b949c5d99b36738c89ac3bab
David Jones <davidthomjones@gmail.com>
2020-02-07 10:34:51 +1000

    Upgraded to latest jQuery version

--- tweak.js
+++ tweak.js
@@ -1,7 +1,9 @@
 <div id="gu_card_intro"></div>
 
     
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+		integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+		crossorigin="anonymous"></script>
 <script src="https://s3.amazonaws.com/filebucketdave/banner.js/cards.js" type="text/javascript"></script>
 <script type="text/javascript" class="gutweak"><![CDATA[
 $.noConflict(); 
