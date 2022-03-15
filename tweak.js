cf0f184ab1f9bd2955fc7884ff010d8740b00934
David Jones <davidthomjones@gmail.com>
2022-03-15 13:25:02 +1000

    Update to latest jquery version and avoid minor clash with Blackboard

--- tweak.js
+++ tweak.js
@@ -1,9 +1,8 @@
 <div id="gu_card_intro"></div>
 
     
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-		integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
-		crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" 
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://s3.amazonaws.com/filebucketdave/banner.js/cards.js" type="text/javascript"></script>
 <script type="text/javascript" class="gutweak"><![CDATA[
 $.noConflict(); 
